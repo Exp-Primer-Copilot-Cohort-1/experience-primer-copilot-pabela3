c6d574203b9754a44986ae5f30d08c284f496783
pabela3 <161430680+pabela3@users.noreply.github.com>
2024-03-01 10:13:43 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,55 +1,32 @@
-//create new server
-var express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose');
-var Comment = require('../models/Comment.js');
+//Create web server
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+var comments = [];
 
-// GET /comments
-router.get('/', function(req, res, next) {
-  Comment.find(function (err, comments) {
-    if (err) return next(err);
-    res.json(comments);
-  });
+const server = http.createServer(function (req, res) {
+    var parsedURL = url.parse(req.url, true);
+    var path = parsedURL.pathname;
+    var query = parsedURL.query;
+    console.log(query);
+    if (path == "/addComment") {
+        console.log("Adding comment");
+        comments.push(query);
+        console.log(comments);
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(JSON.stringify(comments));
+        res.end();
+    } else if (path == "/getComments") {
+        console.log("Getting comments");
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(JSON.stringify(comments));
+        res.end();
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.write("Error: Unknown path");
+        res.end();
+    }
 });
 
-// POST /comments
-router.post('/', function(req, res, next) {
-  Comment.create(req.body, function (err, comment) {
-    if (err) return next(err);
-    res.json(comment);
-  });
-});
-
-// GET /comments/:id
-router.get('/:id', function(req, res, next) {
-  Comment.findById(req.params.id, function (err, comment) {
-    if (err) return next(err);
-    res.json(comment);
-  });
-});
-
-// PUT /comments/:id
-router.put('/:id', function(req, res, next) {
-  Comment.findByIdAndUpdate(req.params.id, req.body, function (err, comment) {
-    if (err) return next(err);
-    res.json(comment);
-  });
-});
-
-// DELETE /comments/:id
-router.delete('/:id', function(req, res, next) {
-  Comment.findByIdAndRemove(req.params.id, req.body, function (err, comment) {
-    if (err) return next(err);
-    res.json(comment);
-  });
-});
-
-module.exports = router;
-```
-- Now let's create the comment model
-```javascript
-// Path: Comment.js
-var mongoose = require('mongoose');
-
-var CommentSchema = new mongoose.Schema({
-  body: String,
+server.listen(8000);
+console.log("Server is running on 8000");
